e8fefa1278a2df1807e5a994d5cc5ba8a7267121
Travis Cao <scaotravis@gmail.com>
2020-06-04 21:05:35 -0500

    optimizing using struct and fns

--- code/optimal-growth_travis-cao.jl
+++ code/optimal-growth_travis-cao.jl
@@ -1,55 +1,81 @@
-using Parameters, Plots #read in necessary packages
+# Computational Bootcamp
+# Problem set 1, Question 7
+# Travis Cao
 
-#global variables instead of structs
-β = 0.99 #discount rate.
-θ = 0.36 #capital share
-δ = 0.025 #capital depreciation
-k_grid = collect(range(1.0, length = 100, stop = 75.0)) #capital grid
-nk = length(k_grid) #number of capital elements
-markov = [0.977 0.023; 0.074 0.926] #markov transition process
-z_grid = [1.25, 0.2] #productivity state grid
-nz = length(z_grid) #number of productivity states
+# Console clearing in Juno
+clearconsole()
 
-#initialize value function and policy functions, again as globals.
-val_func = zeros(nk, nz)
-pol_func = zeros(nk, nz)
+# Change working directory to current file folder
+cd(dirname(@__FILE__))
 
-#Bellman operator. Note the lack of type declarations inthe function -- another exaple of sub-optimal coding
-function Bellman(val_func, pol_func)
+# Use necessary packages
+using Parameters, Plots
+
+# Struct variables and results
+@with_kw struct Primitives
+    β::Float64 = .99 # discount rate
+    θ::Float64 = .36 # capital share
+    δ::Float64 = .025 # capital depreciation rate
+    k_grid::Array{Float64,1} = collect(range(.01, length = 1000, stop = 45.0)) # capital grid
+    nk::Int64 = length(k_grid) # number of capital elements
+    markov::Array{Float64,2} = [0.977 0.023; 0.074 0.926] # markov transition process
+    z_grid::Array{Float64,1} = [1.25, 0.2] # productivity state grid
+    nz::Int64 = length(z_grid) # number of productivity states
+end
+
+@with_kw mutable struct Results
+    val_func::Array{Float64,2} = zeros(Primitives().nk, Primitives().nz)
+    pol_func::Array{Float64,2} = zeros(Primitives().nk, Primitives().nz)
+end
+
+# Bellman operator
+function Bellman(prim::Primitives, res::Results)
+    @unpack β, θ, δ, k_grid, nk, markov, z_grid, nz = prim
     v_next = zeros(nk, nz)
 
-    for i_k = 1:nk, i_z = 1:nz #loop over state space
-        candidate_max = -1e10 #something crappy
-        k, z = k_grid[i_k], z_grid[i_z] #convert state indices to state values
-        budget = z*k^θ + (1-δ)*k #budget given current state. Doesn't this look nice?
+    for i_k = 1:nk, i_z = 1:nz # loop over state space
+        max_util = -Inf # some max_util initialization
+        k, z = k_grid[i_k], z_grid[i_z] # convert state indices to state values
+        budget = z*k^θ + (1-δ)*k #budget in the current state
 
-        for i_kp = 1:nk #loop over choice of k_prime
+        # find k_prime such that max util
+        for i_kp = 1:nk
             kp = k_grid[i_kp]
-            c = budget - kp #consumption
-            if c>0 #check to make sure that consumption is positive
-                val = log(c) + β * sum(val_func[i_kp,:].*markov[i_z, :])
-                if val>candidate_max #check for new max value
-                    candidate_max = val
-                    pol_func[i_k, i_z] = kp #update policy function
+            c = budget - kp # consumption
+            if c > 0 # check to make sure that consumption is positive
+                val = log(c) + β * sum(res.val_func[i_kp, :].*markov[i_z, :])
+                if val > max_util # check for new max value
+                    max_util = val
+                    res.pol_func[i_k, i_z] = kp # update policy function
                 end
             end
         end
-        v_next[i_k, i_z] = candidate_max #update next guess of value function
+
+        v_next[i_k, i_z] = max_util # update next guess of value function
     end
-    v_next, pol_func
+
+    return v_next
 end
 
-#more bad globals
-error = 100
-n = 0
-tol = 1e-3
-while error>tol
-    global n, val_func, error, pol_func #declare that we're using the global definitions of these variables in this loop
-    n+=1
-    v_next, pol_func = Bellman(val_func, pol_func)
-    error = maximum(abs.(val_func - v_next)) #reset error term
-    val_func = v_next #update value function held in results vector
-end
-println("Value function converged in ", n, " iterations.")
+function solve_stochastic_model()
+    prim = Primitives()
+    res = Results()
+    error, n, tol = Inf, 0, 1e-3
 
-#############
+    while error > tol
+        n += 1
+        v_next = Bellman(prim, res)
+        error = maximum(abs.(v_next .- res.val_func))
+        res.val_func = v_next
+    end
+
+    println("Value function converged in ", n, " iterations.")
+    return prim, res
+end
+
+@time primitives, results = solve_stochastic_model()
+val_func = results.val_func
+pol_func = results.pol_func
+
+#### Value function converged in 669 iterations.
+#### 170.615123 seconds (2.14 G allocations: 191.075 GiB, 19.18% gc time)
