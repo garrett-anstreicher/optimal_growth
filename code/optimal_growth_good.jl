9d7c76dfd0a0934a80061c5f94657a3c14047f23
hlim93 <hyunkyeong.lim@wisc.edu>
2020-06-07 23:37:16 -0500

    Previously made mistake by not saving the file so this is the correct one.

--- code/optimal_growth_good.jl
+++ code/optimal_growth_good.jl
@@ -1,53 +1,87 @@
-using Parameters, Plots #read in necessary packages
+using Parameters, Plots
 
-#global variables instead of structs
-β = 0.99 #discount rate.
-θ = 0.36 #capital share
-δ = 0.025 #capital depreciation
-k_grid = collect(range(1.0, length = 1800, stop = 45.0)) #capital grid
-nk = length(k_grid) #number of capital elements
+@with_kw struct Primitives
+    β::Float64 = .99
+    θ::Float64 = .36
+    δ::Float64 = .025
+    k_grid::Array{Float64,1} = collect(range(0.01, length=1800, stop=45))
+    nk::Int64 = length(k_grid)
+end
 
-#initialize value function and policy functions, again as globals.
-val_func = zeros(nk)
-pol_func = zeros(nk)
+@with_kw struct States
+    Z::Array{Float64,2} = [1.25 .2] #[Zg Zb]
+    nz::Int64 = length(Z)
+    Π::Array{Float64,2} = [.977 .023; .074 .926] #[Pgg Pgb; Pbg Pbb]
+end
 
-#Bellman operator. Note the lack of type declarations inthe function -- another exaple of sub-optimal coding
-function Bellman(val_func, pol_func)
-    v_next = zeros(nk)
+mutable struct Results # mutable makes it able to update the contents
+    val_func::Array{Float64,2}
+    pol_func::Array{Float64,2}
+end
 
-    for i_k = 1:nk #loop over state space
-        candidate_max = -1e10 #something crappy
-        k = k_grid[i_k]#convert state indices to state values
-        budget = k^θ + (1-δ)*k #budget given current state. Doesn't this look nice?
+function Solve_model()
+    prim = Primitives()
+    st = States()
+    val_func, pol_func = zeros(prim.nk,st.nz), zeros(prim.nk,st.nz)
+    res = Results(val_func, pol_func)
 
-        for i_kp = 1:nk #loop over choice of k_prime
-            kp = k_grid[i_kp]
-            c = budget - kp #consumption
-            if c>0 #check to make sure that consumption is positive
-                val = log(c) + β * val_func[i_kp]
-                if val>candidate_max #check for new max value
-                    candidate_max = val
-                    pol_func[i_k] = kp #update policy function
-                end
-            end
-        end
-        v_next[i_k] = candidate_max #update next guess of value function
+    error, n = 100, 0
+    while error > .0001
+        @unpack val_func, pol_func = res
+        n+=1 # counter of iter
+
+        v_next, p_next = Bellman(prim, res, st)
+        error = sum(abs.(v_next - val_func))+sum(abs.(p_next - pol_func))
+        res = Results(v_next, p_next)
     end
-    v_next, pol_func
+
+    println("Value function converged in ", n, " iteration.")
+    return prim, res, st
 end
 
-#more bad globals
-error = 100
-n = 0
-tol = 1e-4
-while error>tol
-    global n, val_func, error, pol_func #declare that we're using the global definitions of these variables in this loop
-    n+=1
-    v_next, pol_func = Bellman(val_func, pol_func)
-    error = maximum(abs.(val_func - v_next)) #reset error term
-    val_func = v_next #update value function held in results vector
-    println(n, "  ",  error)
+function Bellman(prim::Primitives, res::Results, st::States)
+    @unpack β, δ, θ, nk, k_grid = prim
+    @unpack Z, nz, Π = st
+    @unpack val_func, pol_func = res
+
+    v_next = zeros(nk,nz)
+    p_next = zeros(nk,nz)
+    for (i_z, z) in enumerate(Z)
+        Pr = Π[i_z,:]
+
+        for (i_k, k_now) in enumerate(k_grid)
+            max_util = log(0)
+            k_next = log(0)
+            budget = z*k_now^θ + (1-δ)*k_now
+
+            for (i_kp, k_temp) in enumerate(k_grid)
+                c_temp = budget - k_temp
+
+                if c_temp > 0
+                    val = log(c_temp) + β*(val_func[i_kp,1]*Pr[1] + val_func[i_kp,2]*Pr[2])
+                else
+                    val = log(0)
+                end
+
+                if val > max_util
+                    max_util = val
+                    k_next = k_temp
+                end
+
+            end
+
+            v_next[i_k,i_z] = max_util
+            p_next[i_k,i_z] = k_next
+        end
+    end
+    return v_next, p_next
 end
-println("Value function converged in ", n, " iterations.")
+
+@time prim, res = Solve_model()
+
+Plots.plot(prim.k_grid, res.val_func[:,1], label="Good State")
+Plots.plot!(prim.k_grid, res.val_func[:,2], label="Bad State")
+Plots.plot!(title="Value Functions", legend=:bottomright, xlabel="Capital")
+savefig("/Volumes/GoogleDrive/My Drive/Coding/Julia/PS/q7_plot.png")
 
 #############
