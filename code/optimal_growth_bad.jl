5f33878db9e84463b00806c0175358be75e3b364
mattcarl22 <42317910+mattcarl22@users.noreply.github.com>
2020-06-09 10:44:34 -0500

    optimal_growth_less_bad
    Updated "bad" code with "less bad" code!

--- code/optimal_growth_bad.jl
+++ code/optimal_growth_bad.jl
@@ -1,53 +1,67 @@
 using Parameters, Plots #read in necessary packages
 
-#global variables instead of structs
-β = 0.99 #discount rate.
-θ = 0.36 #capital share
-δ = 0.025 #capital depreciation
-k_grid = collect(range(1.0, length = 1800, stop = 45.0)) #capital grid
-nk = length(k_grid) #number of capital elements
+@with_kw struct Primitives
+    β::Float64 = 0.99 #discount factor
+    θ::Float64 = 0.36 #production
+    δ::Float64 = 0.025 #depreciation
+    k_grid::Array{Float64,1} = collect(range(0.1, length = 1800, stop= 45.0)) #capital grid
+    nk::Int64 = length(k_grid) #number of capital grid states
+    z_grid::Array{Float64,1} =  [1.25, 0.2] #productivity state grid
+    nz::Int64 = length(z_grid)
+    markov::Array{Float64,2} = [0.977 0.023; 0.074 0.926] #markov transition process
+end
 
-#initialize value function and policy functions, again as globals.
-val_func = zeros(nk)
-pol_func = zeros(nk)
+mutable struct Results
+    val_func::Array{Float64,2} #value function
+    pol_func::Array{Float64,2} #policy function
+end
 
-#Bellman operator. Note the lack of type declarations inthe function -- another exaple of sub-optimal coding
-function Bellman(val_func, pol_func)
-    v_next = zeros(nk)
+# Function to solve the model
+function Solve_model()
+    prim = Primitives()
+    val_func, pol_func = zeros(prim.nk, prim.nz), zeros(prim.nk, prim.nz)
+    res = Results(val_func, pol_func)
 
-    for i_k = 1:nk #loop over state space
+    error, n = 100, 0
+    while error>0.0001 #loop until convergence
+        n+=1
+        v_next, res.pol_func = Bellman(prim, res) #next guess of value function
+        error = maximum(abs.(v_next .- res.val_func)) #check for convergence
+        res.val_func = v_next #update
+        println("Current error: ", error)
+    end
+    println("Value function converged in ", n, " iterations")
+    prim, res
+end
+
+# Bellman operator
+function Bellman(prim::Primitives, res::Results)
+    @unpack β, δ, θ, nk, k_grid, nz, z_grid, markov = prim #unpack primitive structure
+    v_next = zeros(nk,nz) #preallocate next guess of value function
+
+    for i_k = 1:nk, i_z = 1:nz #loop over state space
         candidate_max = -1e10 #something crappy
-        k = k_grid[i_k]#convert state indices to state values
-        budget = k^θ + (1-δ)*k #budget given current state. Doesn't this look nice?
+        k, z = k_grid[i_k], z_grid[i_z] #convert state indices to state values
+        budget = z*k^θ + (1-δ)*k #budget given current state. Doesn't this look nice?
 
-        for i_kp = 1:nk #loop over choice of k_prime
-            kp = k_grid[i_kp]
+        for i_kp = 1:nk #loop over choice of k'
+            kp = k_grid[i_kp] #value of k'
             c = budget - kp #consumption
-            if c>0 #check to make sure that consumption is positive
-                val = log(c) + β * val_func[i_kp]
-                if val>candidate_max #check for new max value
-                    candidate_max = val
-                    pol_func[i_k] = kp #update policy function
+            if c>0 #check to make sure consumption is positive
+                val = log(c) + β * sum(res.val_func[i_kp,:] .* markov[i_z, :]) #compute utility
+                if val > candidate_max #check for new max value
+                    candidate_max = val #reset max value
+                    res.pol_func[i_k, i_z] = kp #update policy function
                 end
             end
         end
-        v_next[i_k] = candidate_max #update next guess of value function
+        v_next[i_k,i_z] = candidate_max #update value function
     end
-    v_next, pol_func
+    v_next, res.pol_func
 end
 
-#more bad globals
-error = 100
-n = 0
-tol = 1e-4
-while error>tol
-    global n, val_func, error, pol_func #declare that we're using the global definitions of these variables in this loop
-    n+=1
-    v_next, pol_func = Bellman(val_func, pol_func)
-    error = maximum(abs.(val_func - v_next)) #reset error term
-    val_func = v_next #update value function held in results vector
-    println(n, "  ",  error)
-end
-println("Value function converged in ", n, " iterations.")
-
-#############
+# The code seems to be running a little slowly... not sure if this is a feature of
+#   the exercise parameters or issue with my solution. Anyways, it runs correctly
+#   after some time.
+@elapsed prim, res = Solve_model() #solve the model.
+Plots.plot(prim.k_grid, res.val_func) #plot value function
