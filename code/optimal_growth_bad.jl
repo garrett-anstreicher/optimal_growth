ddf55fa93efad82b2d3a133d1de6df8e5a14c7cf
asteles <38533358+asteles@users.noreply.github.com>
2022-06-29 08:06:18 -0400

    Update optimal_growth_bad.jl
    Added stochastic process Z as state-variables;
    
    improved running time by adding structures;

--- code/optimal_growth_bad.jl
+++ code/optimal_growth_bad.jl
@@ -1,55 +1,89 @@
-####hello everybody!
+### Ana Sofia Teixeira Teles
 
 using Parameters, Plots #read in necessary packages
 
-#global variables instead of structs
-β = 0.99 #discount rate.
-θ = 0.36 #capital share
-δ = 0.025 #capital depreciation
-k_grid = collect(range(1.0, length = 1800, stop = 45.0)) #capital grid
-nk = length(k_grid) #number of capital elements
+#Used structures here:
 
-#initialize value function and policy functions, again as globals.
-val_func = zeros(nk)
-pol_func = zeros(nk)
+@with_kw struct Primitives
+    β::Float64 = 0.99 #discount rate.
+    θ::Float64 = 0.36 #capital share
+    δ::Float64 = 0.025 #capital depreciation
+    k_grid::Array{Float64,1} = collect(range(1.0, length = 1800, stop = 45.0)) #capital grid
+    nk::Int64 = length(k_grid) #number of capital elements
+    pi::Matrix{Float64} = [0.9770 0.023; 0.074 0.926] #transition matrix
+    z::Array{Float64,1} = [1.25, 0.2] # values of Z in good and bad state
+    nz::Int64 = 2 #number of states Z
+end
+
+#initialize value function and policy functions: changed to structure.
+
+mutable struct Results
+    val_func::Matrix{Float64}
+    pol_func::Matrix{Float64}
+end   
 
 #Bellman operator. Note the lack of type declarations inthe function -- another exaple of sub-optimal coding
-function Bellman(val_func, pol_func)
-    v_next = zeros(nk)
+function Bellman(prim::Primitives, res::Results)
 
-    for i_k = 1:nk #loop over state space
-        candidate_max = -1e10 #something crappy
-        k = k_grid[i_k]#convert state indices to state values
-        budget = k^θ + (1-δ)*k #budget given current state. Doesn't this look nice?
+    @unpack β, θ, δ, k_grid, nk, pi, z, nz = prim #unpack primitive structure
+    v_next = zeros((nk,2)) #preallocate next guess of value function
 
-        for i_kp = 1:nk #loop over choice of k_prime
-            kp = k_grid[i_kp]
-            c = budget - kp #consumption
-            if c>0 #check to make sure that consumption is positive
-                val = log(c) + β * val_func[i_kp]
-                if val>candidate_max #check for new max value
-                    candidate_max = val
-                    pol_func[i_k] = kp #update policy function
+    for i_z = 1:nz #loop over z state Variable
+        zi = z[i_z]
+
+        for i_k = 1:nk #loop over k state space
+            candidate_max = -1e10 #something crappy
+            k = k_grid[i_k]#convert state indices to state values
+            budget = zi*k^θ + (1-δ)*k #budget given current state. Doesn't this look nice?
+
+            for i_kp = 1:nk #loop over choice of k_prime
+                kp = k_grid[i_kp]
+                c = budget - kp #consumption
+
+                if c>0 #check to make sure that consumption is positive
+                    val = log(c) + β * pi[i_z,:]'*res.val_func[i_kp,:]
+
+                    if val>candidate_max #check for new max value
+                        candidate_max = val
+                        res.pol_func[i_k, i_z] = kp #update policy function
+                    end
                 end
             end
+            v_next[i_k,i_z] = candidate_max #update next guess of value function
         end
-        v_next[i_k] = candidate_max #update next guess of value function
+        
     end
-    v_next, pol_func
+    v_next
 end
 
-#more bad globals
-error = 100
-n = 0
-tol = 1e-4
-while error>tol
-    global n, val_func, error, pol_func #declare that we're using the global definitions of these variables in this loop
-    n+=1
-    v_next, pol_func = Bellman(val_func, pol_func)
-    error = maximum(abs.(val_func - v_next)) #reset error term
-    val_func = v_next #update value function held in results vector
-    println(n, "  ",  error)
-end
-println("Value function converged in ", n, " iterations.")
+#Create structure with optimization parameters
 
+mutable struct Optim
+    error::Float64
+    n::Int
+end
+
+tol = 1e-4 
+function solve_model()
+
+    prim = Primitives()
+    val_func, pol_func = zeros((prim.nk,prim.nz)), zeros((prim.nk,prim.nz))
+    res = Results(val_func, pol_func)
+
+     o = Optim(100.0,0)   #initialize optim parameters error, n
+    while o.error>tol
+        o.n+=1
+        v_next = Bellman(prim, res)
+        o.error = maximum(vec(abs.(v_next - res.val_func))) #reset error term: vec operator makes matrix 1-dim array
+        res.val_func = v_next #update value function held in results vector
+
+        println(o.n, "  ",  o.error)
+    end
+
+    println("Value function converged in ", o.n, " iterations.")
+    res #output value function and policy function
+
+end
 #############
+
+solve_model()
